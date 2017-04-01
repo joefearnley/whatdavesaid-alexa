06d28761450ab8acee9d2a3ba96262932369b206
Joe Fearnley <joe.fearnley@gmail.com>
2017-04-01 13:54:57 -0400

    Updating intent file

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+var APP_ID = "";
+var SKILL_NAME = 'What Dave Said';
+
+/**
+ * Array containing feanrley facts.
+ */
+var clips = [
+];
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetClip');
+    },
+    'GetNewClipIntent': function () {
+        this.emit('GetClip');
+    },
+    'GetClip': function () {
+        var randomIndex = Math.floor(Math.random() * feanrleyFacts.length);
+        var clip = clips[randomIndex];
+
+        var speechOutput = "Here's what dave said: " + clip;
+
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = "You can ask what dave said, or, you can say exit... What can I help you with?";
+        var reprompt = "What can I help you with?";
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
